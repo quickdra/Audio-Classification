d47c21a8074a3a3fdb2827dbc4467cb047db01c2
quickdra <rajatvpanchal@gmail.com>
2020-09-14 08:25:24 +0530

    Update Cats_dogs_audio.py

--- Cats_dogs_audio.py
+++ Cats_dogs_audio.py
@@ -12,10 +12,8 @@ tf.random.set_seed(10)
 import librosa
 import time
 import matplotlib.pyplot as plt
-
-
-file_path = '' #File path for the folder where the audio files are located
-csv = 'train_test_split.csv' #CSV file containing the names of the audio files
+file_path = '../audio-cats-and-dogs/' #File path for the folder where the audio files are located
+csv = '../audio-cats-and-dogs/train_test_split.csv' #CSV file containing the names of the audio files
 file_names = pd.read_csv(csv)
 file_names = file_names.fillna(0) # A few columns had null values
 
@@ -25,71 +23,74 @@ file_names = file_names.fillna(0) # A few columns had null values
 #Returns features for every window and label
 #The windows are appended to a file, this is done because as numpy concatenate gets temporally exhausting as large arrays are to be concatenated
 def create_features(file_names, file_path):
-  flag = 0
-  columns = ['train_cat', 'train_dog']
-  w = tables.open_file('window.h5','w')
-  atom1 = tables.Float32Atom()
-  array_w = w.create_earray(w.root, 'data', atom1, (0,20))
-  l = tables.open_file('label.h5','w')
-  atom2 = tables.Float32Atom()
-  array_l = l.create_earray(l.root, 'data', atom2, (0,1))
-  temp = []
-  for col in columns:
-    count = 0
-    for file in file_names[col]:
-      filefp = file_path+file
-      data, fs = soundfile.read(filefp)
-      data = data.reshape(-1,1)
-      wn = np.random.randn(len(data)).reshape(-1,1)
-      data_wn = data + 0.0075*wn
-      scaler.fit(data)
-      data = scaler.transform(data)
-      data_wn = scaler.transform(data_wn)
-      #Creating MFCC features for 50ms(800 data points) window and 50% overlap(400 hop_length)
-      mfcc = librosa.feature.mfcc(y = data.reshape(data.shape[0],), sr = fs, n_fft = 800, hop_length = 400)
-      mfcc_wn = librosa.feature.mfcc(y = data_wn.reshape(data_wn.shape[0],), sr = fs, n_fft = 800, hop_length = 400)
-      #Finally for each of the windows 20 features are extracted
-      mfcc = mfcc.reshape(-1,20)
-      mfcc_wn = mfcc_wn.reshape(-1,20)
-      #Creating labels
-      if col == 'train_cat':
-        x = 0
-      else:
-        x = 1
-      label = np.array([[x] for i in range(mfcc.shape[0])]).reshape(-1,1)
-      label_wn = np.array([[x] for i in range(mfcc_wn.shape[0])]).reshape(-1,1)
-      array_w.append(mfcc)
-      array_w.append(mfcc_wn)
-      array_l.append(label)
-      array_l.append(mfcc_l)
-      count += mfcc.shape[0] + mfcc_wn.shape[0]
-    temp.append(count)
-  n_cat = temp[0]  #number of windows with class label as cat
-  n_dog = temp[1]  #number of windows with class label as dog
-  w.close()
-  l.close()
-  return n_cat, n_dog
-n_cat, n_dog = create_features(file_names, file_path)
-      
-def create_model():
-  lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(0.001, decay_steps = 25*(n_dog+n_cat-20)//128,decay_rate = 1)
-  #A simple bidirectional NN proved to perform well
-  model = tf.keras.Sequential()
-  model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences = True, activation = 'sigmoid'), input_shape = (20,40)))
-  model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(16, activation = 'sigmoid')))
-  model.add(tf.keras.layers.Dense(1))
-  model.add(tf.keras.layers.Activation('sigmoid'))
-  model.compile(loss = tf.keras.losses.BinaryCrossentropy(),optimizer = tf.keras.optimizers.Adam(lr_schedule), metrics = ['accuracy'])
-  model.summary()
-  return model
+    flag = 0
+    columns = ['train_cat', 'train_dog']
+    w = tables.open_file('../window.h5','w')
+    atom1 = tables.Float32Atom()
+    array_w = w.create_earray(w.root, 'data', atom1, (0,20))
+    l = tables.open_file('../label.h5','w')
+    atom2 = tables.Float32Atom()
+    array_l = l.create_earray(l.root, 'data', atom2, (0,1))
+    temp = []
+    scaler = StandardScaler()
+    for col in columns:
+        count = 0
+        for file in file_names[col]:
+            if file == 0:
+                continue
+            filefp = file_path+file
+            data, fs = soundfile.read(filefp)
+            data = data.reshape(-1,1)
+            wn = np.random.randn(len(data)).reshape(-1,1)
+            data_wn = data + 0.0075*wn
+            scaler.fit(data)
+            data = scaler.transform(data)
+            data_wn = scaler.transform(data_wn)
+            #Creating MFCC features for 50ms(800 data points) window and 50% overlap(400 hop_length)
+            mfcc = librosa.feature.mfcc(y = data.reshape(data.shape[0],), sr = fs, n_fft = 800, hop_length = 400)
+            mfcc_wn = librosa.feature.mfcc(y = data_wn.reshape(data_wn.shape[0],), sr = fs, n_fft = 800, hop_length = 400)
+            #Finally for each of the windows 20 features are extracted
+            mfcc = mfcc.reshape(-1,20)
+            mfcc_wn = mfcc_wn.reshape(-1,20)
+            #Creating labels
+            if col == 'train_cat':
+                x = 0
+            else:
+                x = 1
+            label = np.array([[x] for i in range(mfcc.shape[0])]).reshape(-1,1)
+            label_wn = np.array([[x] for i in range(mfcc_wn.shape[0])]).reshape(-1,1)
+            array_w.append(mfcc)
+            array_w.append(mfcc_wn)
+            array_l.append(label)
+            array_l.append(label_wn)
+            count += mfcc.shape[0] + mfcc_wn.shape[0]
+        temp.append(count)
+    n_cat = temp[0]  #number of windows with class label as cat
+    n_dog = temp[1]  #number of windows with class label as dog
+    w.close()
+    l.close()
+    return n_cat, n_dog
 
-#Model_fp ---> file path where to save the model
+n_cat, n_dog = create_features(file_names, file_path)
+
+def create_model():
+    lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(0.001, decay_steps = 25*(n_dog+n_cat-20)//128,decay_rate = 1)
+    #A simple bidirectional NN proved to perform well
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences = True, activation = 'sigmoid'), input_shape = (20,40)))
+    model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(16, activation = 'sigmoid')))
+    model.add(tf.keras.layers.Dense(1))
+    model.add(tf.keras.layers.Activation('sigmoid'))
+    model.compile(loss = tf.keras.losses.BinaryCrossentropy(),optimizer = tf.keras.optimizers.Adam(lr_schedule), metrics = ['accuracy'])
+    model.summary()
+    return model
+model = create_model()
 def train_model(model, n_cat, n_dog):
     #A sequence of 40 windows (1 second audio) is used as a data point
     #The model performs better with more context. (Longer sequence)
     #Hence the model can classify 1 sec of audio as either cat/dog audio
-    w = tables.open_file('window.h5','r')
-    l = tables.open_file('label.h5','r')
+    w = tables.open_file('../window.h5','r')
+    l = tables.open_file('../label.h5','r')
     dog_labels = l.root.data[n_cat:,:]
     dog_data = w.root.data[n_cat:,:].reshape(20, -1)
     index = np.random.randint(0,n_dog-40,n_dog-40)
@@ -125,20 +126,24 @@ def train_model(model, n_cat, n_dog):
 
     callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss',patience = 10)
     history = model.fit(new_dat,new_labels, validation_split = 0.2, batch_size = 128, epochs = 100, callbacks = [callback])
-    model.save(model_fp)
-    #Accuracy plot
-    plt.plot(history.history['accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-    #Loss plot
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-    return
+    model.save('../model.h5')
+
+    return history
+
+history = train_model(model, n_cat, n_dog)
+#Accuracy plot
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+#Loss plot
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
